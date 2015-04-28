e201411567d554e2b4bd235fb34e11672eb74263
zysmiles <haedu-1@haedu-1tekiiMac.local>
2015-04-28 17:08:19 +0800

    测试

---
+++ Card/Card/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Card
+//
+//  Created by haedu-1 on 15/4/28.
+//  Copyright (c) 2015年 haedu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Card/Card/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Card
+//
+//  Created by haedu-1 on 15/4/28.
+//  Copyright (c) 2015年 haedu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //测试
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
